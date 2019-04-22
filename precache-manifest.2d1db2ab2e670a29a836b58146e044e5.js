272af6cac5e5e9bc2af48a6c31197830fe94bb03
Marko <marko.perendic@yahoo.com>
2019-04-22 14:23:31 +0200

    Updates

---
+++ precache-manifest.2d1db2ab2e670a29a836b58146e044e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "66657f5dc34703daceb62cb80cf2f7d4",
+    "url": "/react-page/static/media/image001.66657f5d.png"
+  },
+  {
+    "revision": "b12ec7ca4b0ac9d804dd3172dc87ed02",
+    "url": "/react-page/static/media/Helvetica Inserat LT.b12ec7ca.ttf"
+  },
+  {
+    "revision": "d2b3f9b156d0a463a8af",
+    "url": "/react-page/static/js/runtime~main.a09b50d1.js"
+  },
+  {
+    "revision": "ef58b682156bd1a70983",
+    "url": "/react-page/static/js/main.b89fe068.chunk.js"
+  },
+  {
+    "revision": "cd246355cf7bec960074",
+    "url": "/react-page/static/js/2.2c6d0f31.chunk.js"
+  },
+  {
+    "revision": "ef58b682156bd1a70983",
+    "url": "/react-page/static/css/main.ba7454ee.chunk.css"
+  },
+  {
+    "revision": "35643168e1e6361943c95b591cf25fd8",
+    "url": "/react-page/index.html"
+  }
+];
